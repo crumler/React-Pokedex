27b0577c0af37021bb81d03c4908611f14a92143
Christopher Rumler <crumler@dminc.com>
2021-03-08 17:32:59 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function App() {
+
+  const [pokemons, setPokemons] = useState([
+    { id: 1, name: 'Bulbasaur' },
+    { id: 2, name: 'Charmander' },
+    { id: 3, name: 'Squirtle' }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Dropdown>
+        <Dropdown.Toggle variant="danger" id="dropdown-basic">
+          Pok&egrave;mon
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          {pokemons.map((pokemon) => 
+          <Dropdown.Item href="#/action-1">{pokemons.name}</Dropdown.Item>
+          )}
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 }
