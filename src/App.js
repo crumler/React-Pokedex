df71042b759f432e113a78605598d61b0b15b788
Christopher Rumler <crumler@dminc.com>
2021-03-09 07:32:19 -0500

    refined fetch url

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 import Dropdown from 'react-bootstrap/Dropdown';
 
+
+
 function App() {
 
-  const [pokemons, setPokemons] = useState([
-    { id: 1, name: 'Bulbasaur' },
-    { id: 2, name: 'Charmander' },
-    { id: 3, name: 'Squirtle' }
-  ]);
+  // const [pokemons, setPokemons] = useState([
+  //   { id: 1, name: 'Bulbasaur' },
+  //   { id: 2, name: 'Charmander' },
+  //   { id: 3, name: 'Squirtle' }
+  // ]);
+  
+  // Axios fetch call
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+  const [pokemons, setPokemons] = useState('');
+
+  useEffect(() => {
+    axios.get(url)
+    .then(response => {
+      setPokemons(response.data)
+      console.log(response, pokemons)
+    });
+  }, []);
+
   return (
     <div className="App">
       <Dropdown>
@@ -23,17 +39,17 @@ function App() {
         </Dropdown.Menu>
       </Dropdown>
 
-      <Dropdown>
+      {/* <Dropdown>
         <Dropdown.Toggle variant="danger" id="dropdown-basic">
           Pok&egrave;mon
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          {pokemons.map((pokemon) => 
-          <Dropdown.Item href="#/action-1">{pokemons.name}</Dropdown.Item>
-          )}
+          const listPokemon = pokemons.map((pokemon, i) => {
+          <Dropdown.Item href="#/action-1" key={"poke_" + [i]}>{pokemons.name}</Dropdown.Item>
+})
         </Dropdown.Menu>
-      </Dropdown>
+      </Dropdown> */}
     </div>
   );
 }
