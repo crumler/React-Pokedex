17ebdc1edca210ad48187cc105f700c343b8b8a3
Christopher Rumler <crumler@dminc.com>
2021-03-12 09:36:32 -0500

    created PokemonList component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,25 @@
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import './App.css';
-import Dropdown from 'react-bootstrap/Dropdown';
-
-
+import PokemonList from './components/PokemonList';
 
 function App() {
-
-  // const [pokemons, setPokemons] = useState([
-  //   { id: 1, name: 'Bulbasaur' },
-  //   { id: 2, name: 'Charmander' },
-  //   { id: 3, name: 'Squirtle' }
-  // ]);
   
   // Axios fetch call
   const url = 'https://pokeapi.co/api/v2/pokemon';
-  const [pokemons, setPokemons] = useState('');
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
     axios.get(url)
     .then(response => {
-      setPokemons(response.data)
-      console.log(response, pokemons)
+      setPokemons(response.data.results)
+      console.log(response.data.results)
     });
   }, []);
 
   return (
     <div className="App">
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Dropdown Button
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-
-      {/* <Dropdown>
-        <Dropdown.Toggle variant="danger" id="dropdown-basic">
-          Pok&egrave;mon
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          const listPokemon = pokemons.map((pokemon, i) => {
-          <Dropdown.Item href="#/action-1" key={"poke_" + [i]}>{pokemons.name}</Dropdown.Item>
-})
-        </Dropdown.Menu>
-      </Dropdown> */}
+      <PokemonList pokemons={pokemons} />
     </div>
   );
 }
---
+++ src/components/PokemonList.js
@@ -0,0 +1,19 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function PokemonList( {pokemons} ) {
+    return (
+        <div>
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    List of Pok&egrave;mon
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                    {pokemons.map((pokemon => {
+                        return <Dropdown.Item href="#" key={pokemon}>{pokemon.name[0].toUpperCase() + pokemon.name.substring(1)}</Dropdown.Item>
+                    }))}
+                </Dropdown.Menu>
+            </Dropdown>
+        </div>
+    )
+};
